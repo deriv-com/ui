a92d62c21228e8aa4d3a4530200e80beae5d3229
Aum Bhatt <aum@deriv.com>
2024-01-26 11:26:23 +0400

    chore: 📄 added JSDoc

--- lib/hooks/useDevice.ts
+++ lib/hooks/useDevice.ts
@@ -1,5 +1,6 @@
 import { useMediaQuery } from "./useMediaQuery";
 
+/** A custom hook to check for the client device and determine the layout to be rendered */
 export const useDevice = () => {
     const isDesktop = useMediaQuery(`(min-width: 1024px)`);
     const isMobile = useMediaQuery(`(max-width: 768px)`);
