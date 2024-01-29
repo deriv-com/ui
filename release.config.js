fca1c7ee16df4aa94533cd042439f99d0adc3d4e
Ali(Ako) Hosseini <ali.hosseini@deriv.com>
2024-01-29 18:21:13 +0800

    ci: update project repo path

--- release.config.js
+++ release.config.js
@@ -9,7 +9,7 @@ module.exports = {
     { name: 'beta', prerelease: true },
     { name: 'alpha', prerelease: true },
   ],
-  repositoryUrl: 'git@github.com:deriv-com/quill-icons.git',
+  repositoryUrl: 'git@github.com:deriv-com/ui.git',
   plugins: [
     [
       '@semantic-release/commit-analyzer',
