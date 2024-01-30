43d48a36171928236005620d0fc6668551d28ccf
shayan khaleghparast <iman@regentmarkets.com>
2024-01-30 10:29:01 +0800

    fix: removed duplicated import statement

--- lib/main.ts
+++ lib/main.ts
@@ -6,5 +6,4 @@ export { Tab, Tabs } from "./components/Tabs";
 export { Text } from "./components/Text";
 export { Tooltip } from "./components/Tooltip";
 export {ToggleSwitch} from "./components/ToggleSwitch"
-export { Input } from "./components/Input";
 
