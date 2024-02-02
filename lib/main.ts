052882bcaec4158f6fa30654b6156c3a25c941a4
Aum Bhatt <aum@deriv.com>
2024-02-02 11:52:42 +0400

    chore: update main.ts imports

--- lib/main.ts
+++ lib/main.ts
@@ -1,9 +1,8 @@
 export { Button } from "./components/Button";
 export { Input } from "./components/Input";
-export { PageLayout } from "./components/Layout";
+export { PageLayout } from "./components/PageLayout";
 export { Loader } from "./components/Loader";
 export { Tab, Tabs } from "./components/Tabs";
 export { Text } from "./components/Text";
 export { Tooltip } from "./components/Tooltip";
-export {ToggleSwitch} from "./components/ToggleSwitch"
-
+export { ToggleSwitch } from "./components/ToggleSwitch";
