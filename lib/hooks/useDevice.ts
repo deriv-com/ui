88f2c6e55d2c121641f75a36af5af8c897f13c33
shayan khaleghparast <iman@regentmarkets.com>
2024-02-05 10:04:43 +0800

    fix: fixed breakpoints overlap issue

--- lib/hooks/useDevice.ts
+++ lib/hooks/useDevice.ts
@@ -1,21 +1,16 @@
 import { useMediaQuery } from "./useMediaQuery";
 
-const breakpoints = {
-    mobile: '600px',
-    desktop: '1280px',
-};
-
 /** A custom hook to check for the client device and determine the layout to be rendered */
 export const useDevice = () => {
-    const isDesktop = useMediaQuery(`(min-width: ${breakpoints.desktop})`);
-    const isMobile = useMediaQuery(`(max-width: ${breakpoints.mobile})`);
-    const isTablet = useMediaQuery(`(min-width: ${breakpoints.mobile}) and (max-width: ${breakpoints.desktop})`);
+    const isDesktop = useMediaQuery(`(min-width: 1280px)`);
+    const isMobile = useMediaQuery(`(max-width: 600px)`);
+    const isTablet = useMediaQuery(`(min-width: 601px) and (max-width: 1279px)`);
     return {
         /** returns Larger screen tablets [min-width: 1280px] */
         isDesktop,
         /**  returns Smaller screen tablets [max-width: 600px] */
         isMobile,
-        /** returns Larger screen phones and smaller screen desktop [min-width: 600px and max-width: 1280px] */
+        /** returns Larger screen phones and smaller screen desktop [min-width: 601px and max-width: 1279px] */
         isTablet
     };
 };
