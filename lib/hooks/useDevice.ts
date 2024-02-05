68db4f950efbd121f1af9a90032a6528a5e95551
shayan khaleghparast <iman@regentmarkets.com>
2024-02-05 09:54:44 +0800

    refactor: updated breakpoints in useDevice hook

--- lib/hooks/useDevice.ts
+++ lib/hooks/useDevice.ts
@@ -1,11 +1,21 @@
 import { useMediaQuery } from "./useMediaQuery";
 
+const breakpoints = {
+    mobile: '600px',
+    desktop: '1280px',
+};
+
 /** A custom hook to check for the client device and determine the layout to be rendered */
 export const useDevice = () => {
-    const isDesktop = useMediaQuery(`(min-width: 1024px)`);
-    const isMobile = useMediaQuery(`(max-width: 768px)`);
+    const isDesktop = useMediaQuery(`(min-width: ${breakpoints.desktop})`);
+    const isMobile = useMediaQuery(`(max-width: ${breakpoints.mobile})`);
+    const isTablet = useMediaQuery(`(min-width: ${breakpoints.mobile}) and (max-width: ${breakpoints.desktop})`);
     return {
+        /** returns Larger screen tablets [min-width: 1280px] */
         isDesktop,
+        /**  returns Smaller screen tablets [max-width: 600px] */
         isMobile,
+        /** returns Larger screen phones and smaller screen desktop [min-width: 600px and max-width: 1280px] */
+        isTablet
     };
 };
