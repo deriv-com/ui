2172f861dd6b44d158a87def3820a0883feda156
aizad-deriv <aizad@deriv.com>
2024-02-07 16:50:07 +0800

    fix:add full width inside of passwordinput

--- lib/components/Input/index.tsx
+++ lib/components/Input/index.tsx
@@ -1,10 +1,4 @@
-import React, {
-  CSSProperties,
-  ComponentProps,
-  ReactNode,
-  Ref,
-  forwardRef,
-} from "react";
+import React, { ComponentProps, ReactNode, Ref, forwardRef } from "react";
 import clsx from "clsx";
 import HelperMessage from "./HelperMessage";
 import "./Input.scss";
