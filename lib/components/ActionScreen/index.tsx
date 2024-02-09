ec8d433acbee0de5ea593541e530b72b2994a192
Aum Bhatt <aum@deriv.com>
2024-02-09 15:22:42 +0400

    chore: :scroll: added storybook for ActionScreen

--- lib/components/ActionScreen/index.tsx
+++ lib/components/ActionScreen/index.tsx
@@ -3,7 +3,8 @@ import { Text } from '../Text';
 import './ActionScreen.scss';
 
 type TProps = {
-    description: ReactNode;
+    children?: ReactNode,
+    description?: ReactNode;
     descriptionSize?: ComponentProps<typeof Text>['size'];
     icon?: ReactNode;
     renderButtons?: () => ReactNode;
@@ -12,9 +13,8 @@ type TProps = {
 };
 
 /**
- * Generic component to display status / action screen / final screen
- * As its common and repeated in many places,
- * at the moment of writing this, there are already 3 different patterns use to display ex
+ * Generic component to display status / action screen / final screen.
+ * (`Icon`, `Title`, `Description`, `CTA`)
  */
 export const ActionScreen: React.FC<PropsWithChildren<TProps>> = ({
     children,
