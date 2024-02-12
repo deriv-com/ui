0d6f4a80fc389062f82d404f0fe7b5eb3af13435
Aum Bhatt <aum@deriv.com>
2024-02-12 11:11:50 +0400

    chore: renamed and changed type for renderButton prop

--- lib/components/ActionScreen/index.tsx
+++ lib/components/ActionScreen/index.tsx
@@ -1,48 +1,57 @@
-import React, { ComponentProps, isValidElement, PropsWithChildren,  ReactNode } from 'react';
-import { Text } from '../Text';
-import './ActionScreen.scss';
+import React, {
+    ComponentProps,
+    isValidElement,
+    PropsWithChildren,
+    ReactNode,
+} from "react";
+import { Text } from "../Text";
+import "./ActionScreen.scss";
 
 type TProps = {
-    children?: ReactNode,
+    actionButtons?: JSX.Element;
+    children?: ReactNode;
     description?: ReactNode;
-    descriptionSize?: ComponentProps<typeof Text>['size'];
+    descriptionSize?: ComponentProps<typeof Text>["size"];
     icon?: ReactNode;
-    renderButtons?: () => ReactNode;
     title?: ReactNode;
-    titleSize?: ComponentProps<typeof Text>['size'];
+    titleSize?: ComponentProps<typeof Text>["size"];
 };
 
 /**
  * Generic component to display status / action screen / final screen.
- * (`Icon`, `Title`, `Description`, `CTA`)
+ * @param {TProps.actionButtons} actionButtons Button / group of buttons for CTA.
+ * @param {TProps.description} description Description content to be displayed as the description.
+ * @param {TProps.descriptionSize} descriptionSize Font-size of the description.
+ * @param {TProps.title} title Title content to be displayed.
+ * @param {TProps.titleSize} titleSize Font-size of the title.
  */
 export const ActionScreen: React.FC<PropsWithChildren<TProps>> = ({
+    actionButtons,
     children,
     description,
-    descriptionSize = 'md',
+    descriptionSize = "md",
     icon,
-    renderButtons,
     title,
-    titleSize = 'md',
+    titleSize = "md",
 }) => {
     return (
-        <div className='derivs-action-screen'>
+        <div className="derivs-action-screen">
             {icon}
-            <div className='derivs-action-screen__info'>
+            <div className="derivs-action-screen__info">
                 {title && (
-                    <Text align='center' size={titleSize} weight='bold'>
+                    <Text align="center" size={titleSize} weight="bold">
                         {title}
                     </Text>
                 )}
                 {isValidElement(description) ? (
                     description
                 ) : (
-                    <Text align='center' size={descriptionSize}>
+                    <Text align="center" size={descriptionSize}>
                         {description}
                     </Text>
                 )}
             </div>
-            {renderButtons?.()}
+            {actionButtons}
             {children}
         </div>
     );
