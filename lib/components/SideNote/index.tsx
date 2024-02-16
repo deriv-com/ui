dec83939c0e9c470839937f702eb3737c79e3a88
Niloofar <niloofar.sadeghi@regentmarkets.com>
2024-02-16 12:13:23 +0800

    feat: added action button to side note component

--- lib/components/SideNote/index.tsx
+++ lib/components/SideNote/index.tsx
@@ -1,34 +1,62 @@
-import { ComponentProps, PropsWithChildren } from "react";
+import {
+  ComponentProps,
+  MouseEventHandler,
+  PropsWithChildren,
+  ReactElement,
+} from "react";
+import { ChevronRightIcon } from "./ChevronRightIcon";
 import { Text } from "../Text";
 import "./SideNote.scss";
+import clsx from "clsx";
 
-interface SideNoteProps extends ComponentProps<"div"> {
-  title?: string;
+type SideNoteProps = Omit<ComponentProps<"div">, "className"> & {
+  title?: string | ReactElement;
   titleSize?: ComponentProps<typeof Text>["size"];
-  sideNoteFooter?: JSX.Element;
-}
+  className?: HTMLDivElement["className"];
+  actionClick?: MouseEventHandler<HTMLButtonElement>;
+  actionClassName?: HTMLDivElement["className"];
+  actionLabel?: string | ReactElement;
+};
 
 /**
  * This component is a design feature that displays extra information next to the main text, providing additional details or comments.
- * @param {SideNoteProps.title} title - Title content to be displayed.
- * @param {SideNoteProps.titleSize} titleSize - Defining title Font-size.
- * @param {SideNoteProps.sideNoteFooter} sideNoteFooter - Action buttons or links like learn more...
- * @returns {React.JSX.Element} - returns the SideNote component
+ * @param {SideNoteProps.title} title - Display the title content.
+ * @param {SideNoteProps.titleSize} titleSize - Set the font-size for the title.
+ * @param {SideNoteProps.className} className - Add an extra className to the container.
+ * @param {SideNoteProps.actionClick} actionClick - An onclick handler for the action button.
+ * @param {SideNoteProps.actionClassName} actionClassName - Add an extra className to the action button.
+ * @param {SideNoteProps.actionLabel} actionLabel - Display the label of the action.
+ * @returns {React.JSX.Element} - Returns the SideNote component.
  */
 export const SideNote = ({
   children,
   title,
   titleSize = "sm",
-  sideNoteFooter,
+  className,
+  actionClick,
+  actionClassName,
+  actionLabel,
   ...props
 }: PropsWithChildren<SideNoteProps>): React.JSX.Element => (
-  <div className="deriv-side-note" {...props}>
+  <div className={clsx("deriv-side-note", className)} {...props}>
     {title && (
       <Text size={titleSize} align="center" weight="bold">
         {title}
       </Text>
     )}
+
     <div>{children}</div>
-    {sideNoteFooter}
+
+    {actionClick && (
+      <button
+        className={clsx("deriv-side-note__action", actionClassName)}
+        onClick={actionClick}
+      >
+        <Text color="red" size="xs">
+          {actionLabel}
+        </Text>
+        <ChevronRightIcon />
+      </button>
+    )}
   </div>
 );
--- src/stories/SideNote.stories.tsx
+++ src/stories/SideNote.stories.tsx
@@ -1,26 +1,35 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { SideNote, Text } from "../../lib/main";
+import { SideNote } from "../../lib/main";
 
 const meta = {
   title: "Components/SideNote",
   component: SideNote,
   args: {
     title: "This is a test title",
-    titleSize: "md",
-    sideNoteFooter: <a href="/">Learn more</a>,
-    children: <Text>This is test note!</Text>,
+    titleSize: "sm",
+    className: "",
+    actionLabel: "Learn more",
+    actionClick: () => console.log("Test action"),
+    actionClassName: "",
   },
   argTypes: {
-    title: {
+    title: { control: false, description: "Display the title content." },
+    titleSize: { description: "Set the font-size for the title." },
+    className: {
       control: false,
-      description: "Contains the title to be shown.",
+      description: "Add an extra className to the container.",
     },
-    titleSize: {
-      description: "Sets the title text size.",
-    },
-    sideNoteFooter: {
+    actionLabel: {
       control: false,
-      description: "A prop which can be passed with a single HTML Element",
+      description: "Display the label of the action.",
+    },
+    actionClick: {
+      control: false,
+      description: "An onclick handler for the action button.",
+    },
+    actionClassName: {
+      control: false,
+      description: "Add an extra className to the action button.",
     },
   },
   parameters: { layout: "centered" },
@@ -32,4 +41,15 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   name: "SideNote",
+  render: () => (
+    <div style={{ width: "300px" }}>
+      <SideNote
+        title="Test title"
+        actionLabel="Learn more"
+        actionClick={() => console.log("test click!")}
+      >
+        <p style={{ margin: "10px 0" }}>This is a test note</p>
+      </SideNote>
+    </div>
+  ),
 };
