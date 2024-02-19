f2a92c51aee493c6c4dec6bd8a86f4bd0a7bc8cd
shayan khaleghparast <iman@regentmarkets.com>
2024-02-19 11:08:40 +0800

    feat: setup jest and rtl

---
+++ babel.config.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+    presets: [["@babel/preset-env", { targets: { esmodules: true } }],
+    ["@babel/preset-react", { runtime: "automatic" }],
+        "@babel/preset-typescript",],
+};
