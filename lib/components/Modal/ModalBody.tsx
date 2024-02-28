d7d3323a4c130bc3bead05e16084f72c94475533
shayan khaleghparast <iman@regentmarkets.com>
2024-02-28 11:14:59 +0800

    chore: added minor changes

--- lib/components/Modal/ModalBody.tsx
+++ lib/components/Modal/ModalBody.tsx
@@ -1,7 +1,7 @@
-import { PropsWithChildren, HtmlHTMLAttributes } from 'react';
+import { ComponentProps } from 'react';
 import clsx from 'clsx';
 
-export const ModalBody = ({ children,className, ...rest }: PropsWithChildren<HtmlHTMLAttributes<HTMLDivElement>>): React.JSX.Element => {
+export const ModalBody = ({ children,className, ...rest }: ComponentProps<"div">)=> {
     return (
         <div className={clsx('deriv-modal__body', className)} {...rest}>
             {children}
--- lib/components/Modal/ModalFooter.tsx
+++ lib/components/Modal/ModalFooter.tsx
@@ -1,7 +1,7 @@
-import { HtmlHTMLAttributes, PropsWithChildren } from 'react';
+import { ComponentProps } from 'react';
 import clsx from 'clsx';
 
-export const ModalFooter = ({ children, className, ...rest }: PropsWithChildren<HtmlHTMLAttributes<HTMLDivElement>>) => {
+export const ModalFooter = ({ children, className, ...rest }: ComponentProps<"div">) => {
     return (
         <div className={clsx(`deriv-modal__footer`, className)} {...rest}>
             {children}
