44d4c813f00ce0351e35ac12abcf298e19398548
aizad-deriv <aizad@deriv.com>
2024-03-06 15:18:00 +0800

    chore: made improvements for Accordion component

--- lib/components/Accordion/index.tsx
+++ lib/components/Accordion/index.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import Chevron from "./Chevron.svg";
+import { memo, useEffect, useRef, useState } from "react";
 import clsx from "clsx";
+import Chevron from "./Chevron.svg";
 import "./Accordion.scss";
 
 type AccordionSectionProps = {
@@ -12,61 +12,79 @@ type AccordionSectionProps = {
 
 type AccordionProps = { sections: AccordionSectionProps["section"] };
 
-const AccordionSection = ({
-    section,
-    isActiveSection,
-    setActiveIndex,
-    sectionIndex,
-}: AccordionSectionProps) => {
-    const toggleSection = () => {
-        const nextIndex = isActiveSection ? null : sectionIndex;
-        setActiveIndex(nextIndex);
-    };
+const AccordionSection = memo(
+    ({
+        section,
+        isActiveSection,
+        setActiveIndex,
+        sectionIndex,
+    }: AccordionSectionProps) => {
+        const toggleSection = () => {
+            const nextIndex = isActiveSection ? null : sectionIndex;
+            setActiveIndex(nextIndex);
+        };
 
-    return (
-        <div>
-            <div
-                className={clsx("deriv-accordion__header", {
-                    "deriv-accordion__header--active": isActiveSection,
-                })}
-                onClick={toggleSection}
-            >
-                <div>{section.title}</div>
-                <div className="deriv-accordion__icon">
-                    <img
-                        src={Chevron}
-                        className={clsx("deriv-accordion__icon", {
-                            "deriv-accordion__icon--active": isActiveSection,
-                        })}
-                    />
+        const [height, setHeight] = useState("");
+        const heightRef = useRef<HTMLDivElement>(null);
+
+        useEffect(() => {
+            const scrollHeight = heightRef.current?.scrollHeight;
+            setHeight(`${scrollHeight}px`);
+        }, [isActiveSection]);
+
+        return (
+            <div className="deriv-accordion__wrapper">
+                <div
+                    className={clsx("deriv-accordion__header", {
+                        "deriv-accordion__header--active": isActiveSection,
+                    })}
+                >
+                    <div>{section.title}</div>
+                    <div
+                        className="deriv-accordion__icon"
+                        onClick={toggleSection}
+                    >
+                        <img
+                            src={Chevron}
+                            className={clsx("deriv-accordion__icon", {
+                                "deriv-accordion__icon--active":
+                                    isActiveSection,
+                            })}
+                        />
+                    </div>
                 </div>
-            </div>
-            {isActiveSection && (
                 <div
                     className={clsx("deriv-accordion__content", {
-                        "deriv-accordion__content": isActiveSection,
+                        "deriv-accordion__content--active": isActiveSection,
                     })}
+                    ref={heightRef}
+                    style={{ maxHeight: isActiveSection ? height : "0px" }}
                 >
                     {section.content}
                 </div>
-            )}
-        </div>
-    );
-};
+            </div>
+        );
+    },
+);
 
 export const Accordion = ({ sections }: AccordionProps) => {
     const [activeIndex, setActiveIndex] = useState<number | null>(null);
     return (
         <div className="deriv-accordion">
-            {sections.map((section, index) => (
-                <AccordionSection
-                    section={section}
-                    key={index}
-                    isActiveSection={index === activeIndex}
-                    setActiveIndex={setActiveIndex}
-                    sectionIndex={index}
-                />
-            ))}
+            {sections.map(
+                (
+                    section: { title: string; content: string },
+                    sectionIndex: number,
+                ) => (
+                    <AccordionSection
+                        section={section}
+                        key={sectionIndex}
+                        isActiveSection={sectionIndex === activeIndex}
+                        setActiveIndex={setActiveIndex}
+                        sectionIndex={sectionIndex}
+                    />
+                ),
+            )}
         </div>
     );
 };
