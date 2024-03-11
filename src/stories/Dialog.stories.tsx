d457a8084d303f176062e16605860b47b1d8dc98
yaswanth-deriv <yaswanth@regentmarkets.com>
2024-03-11 12:03:24 +0400

    feat: added stories for dialog component

--- src/stories/Dialog.stories.tsx
+++ src/stories/Dialog.stories.tsx
@@ -1,19 +1,92 @@
-import { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react";
 import { Dialog } from "../../lib/main";
+import { Button } from "../../lib/components/Button";
+
 
 const meta = {
-    title: 'Components/Dialog',
+    title: "Components/Dialog",
     component: Dialog,
-} satisfies Meta<typeof Dialog>
-export default meta;
-
-type Story = StoryObj<typeof meta>
-
-
-export const BasicDialog: Story = {
+    parameters: {
+        layout: "centered",
+    },
+    tags: ["autodocs"],
     args: {
-        title: 'Dialog Title',
-        bodyChildren: <p>This is the body of the dialog.</p>,
-        footerChildren: <button>Close</button>,
+        children: <p>This is basic dialog</p>,
+    },
+    argTypes: {
+        children: {
+            description: "The content of the modal.you can use the `Dialog.Header`, `Dialog.Body`, and `Dialog.Footer` components to structure the content of the modal.",
+            control: false
+        },
     }
-}
+} satisfies Meta<typeof Dialog>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    name: "Default Dialog",
+    render : ()=>{
+        return (
+            <>
+            <Dialog>
+                <Dialog.Header>title</Dialog.Header>
+                <Dialog.Body>body</Dialog.Body>
+                <Dialog.Footer>
+                    <Button color="black">cancel</Button>
+                    <Button>ok</Button>
+                </Dialog.Footer>
+            </Dialog>
+            </>
+        )
+    }
+};
+
+export const HideHeaderCloseIcon: Story = {
+    name: "Hide Close Icon",
+    render : ()=>{
+        return (
+            <>
+            <Dialog>
+                <Dialog.Header hideCloseIcon >title</Dialog.Header>
+                <Dialog.Body>body</Dialog.Body>
+                <Dialog.Footer>
+                    <Button color="black">cancel</Button>
+                    <Button>ok</Button>
+                </Dialog.Footer>
+            </Dialog>
+            </>
+        )
+    }
+};
+
+export const NoHeader: Story = {
+    name: "Hide Header",
+    render : ()=>{
+        return (
+            <>
+            <Dialog>
+                <Dialog.Body>body</Dialog.Body>
+                <Dialog.Footer>
+                    <Button color="black">cancel</Button>
+                    <Button>ok</Button>
+                </Dialog.Footer>
+            </Dialog>
+            </>
+        )
+    }
+};
+
+export const NoFooter: Story = {
+    name: "Hide Footer",
+    render : ()=>{
+        return (
+            <>
+            <Dialog>
+                <Dialog.Header>title</Dialog.Header>
+                <Dialog.Body>body</Dialog.Body>
+            </Dialog>
+            </>
+        )
+    }
+};
