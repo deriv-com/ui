5f71fcbf9a43eaeed96638e73b6cb1870eff5a70
shayan khaleghparast <iman@regentmarkets.com>
2024-03-12 11:38:37 +0800

    fix: fixed dialog title not showing bug

--- src/components/Dialog/DialogHeader.tsx
+++ src/components/Dialog/DialogHeader.tsx
@@ -1,18 +1,17 @@
-import React,{ ReactNode } from "react"
+import React from "react"
 import { Modal } from "../Modal"
 import { Text } from "../Text"
 
 type TDialogHeaderProps = Omit<React.ComponentProps<typeof Modal.Header>,"hideBorder"> & {
     hideCloseIcon?: boolean;
-    title?: ReactNode;
     textClassName?: string
 }
 
-export const DialogHeader = ({ className, hideCloseIcon = true, title, textClassName,...rest }: TDialogHeaderProps) => {
+export const DialogHeader = ({ children, className, hideCloseIcon = true, textClassName,...rest }: TDialogHeaderProps) => {
     return (
         <Modal.Header hideBorder hideCloseIcon={hideCloseIcon} className={className} {...rest}>
             <Text as='div' weight='bold' className={textClassName}>
-                {title}
+                {children}
             </Text>
         </Modal.Header>
     )
