b2c341d88c33dbb2b6bf37caf45df84a76aa30f9
shayan khaleghparast <iman@regentmarkets.com>
2024-03-12 11:37:17 +0800

    docs: improved storybook and fixed minor bugs

--- stories/Dialog.stories.tsx
+++ stories/Dialog.stories.tsx
@@ -1,6 +1,8 @@
 import type { Meta, StoryObj } from "@storybook/react";
+
 import { Dialog } from "../src/main";
-import { Button } from "../src/main";
+import { Button } from "../src/components/Button";
+import { useEffect, useState } from "react";
 
 
 const meta = {
@@ -11,13 +13,34 @@ const meta = {
     },
     tags: ["autodocs"],
     args: {
-        children: <p>This is basic dialog</p>,
-        isOpen:true
+        children: <Button>Open Dialog</Button>,
+        isOpen: false,
+        onRequestClose: () => { },
     },
     argTypes: {
         children: {
-            description: "The content of the modal.you can use the `Dialog.Header`, `Dialog.Body`, and `Dialog.Footer` components to structure the content of the modal.",
+            description: "The content of the Dialog.you can use the `Dialog.Header`, `Dialog.Body`, and `Dialog.Footer` components to structure the content of the Dialog.",
             control: false
+        },
+        isOpen: {
+            description: "controls the visibility of the Dialog",
+            control: {
+                type: "boolean",
+            },
+        },
+        onRequestClose: {
+            description: "Callback function to close the Dialog",
+            control: false,
+        },
+        ariaHideApp: {
+            description: "Boolean indicating if the appElement should be hidden. check this link for more details: https://reactcommunity.org/react-Dialog/accessibility/",
+            control: {
+                type: "boolean",
+            },
+        },
+        style:{
+            description: "overrides the default styles for b oth Dialog content and overlay, divided into overlay and content styles.",
+            control: false,
         },
     }
 } satisfies Meta<typeof Dialog>;
@@ -27,10 +50,27 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
     name: "Default Dialog",
-    render : ()=>{
+    args: {
+        isOpen: false,
+        ariaHideApp: false,
+        shouldCloseOnOverlayClick: true,
+    },
+    render : (args)=>{
+        const [isOpen, setIsOpen] = useState(args.isOpen);
+
+        useEffect(() => {
+            setIsOpen(args.isOpen);
+        }, [args.isOpen]);
         return (
             <>
-            <Dialog isOpen={true}>
+            <Button onClick={() => setIsOpen(true)}>Open Dialog</Button>
+            <Dialog
+                {...args}
+                isOpen={isOpen}
+                shouldCloseOnOverlayClick={args.shouldCloseOnOverlayClick}
+                onRequestClose={() => setIsOpen(false)}
+                shouldCloseOnEsc={false}
+                >
                 <Dialog.Header>title</Dialog.Header>
                 <Dialog.Body>body</Dialog.Body>
                 <Dialog.Footer>
@@ -43,13 +83,70 @@ export const Default: Story = {
     }
 };
 
-export const HideHeaderCloseIcon: Story = {
-    name: "Hide Close Icon",
-    render : ()=>{
+export const DialogWithoutOverlay: Story = {
+    name: "Disable Overlay",
+    args: {
+        isOpen: false,
+        onRequestClose: () => { },
+        ariaHideApp: false,
+        shouldCloseOnOverlayClick: false,
+    },
+    render : (args)=>{
+        const [isOpen, setIsOpen] = useState(args.isOpen);
+
+        useEffect(() => {
+            setIsOpen(args.isOpen);
+        }, [args.isOpen]);
         return (
             <>
-            <Dialog isOpen={true}>
-                <Dialog.Header hideCloseIcon >title</Dialog.Header>
+            <Button onClick={() => setIsOpen(true)}>Open Dialog</Button>
+            <Dialog
+                {...args}
+                isOpen={isOpen}
+                shouldCloseOnOverlayClick={args.shouldCloseOnOverlayClick}
+                onRequestClose={() => setIsOpen(false)}
+                style={{
+                    overlay:{
+                        backgroundColor:'none'
+                    }
+                }}
+                >
+                <Dialog.Header hideCloseIcon={false} onRequestClose={() => setIsOpen(false)} >title</Dialog.Header>
+                <Dialog.Body>body</Dialog.Body>
+                <Dialog.Footer>
+                    <Button color="black">cancel</Button>
+                    <Button>ok</Button>
+                </Dialog.Footer>
+            </Dialog>
+            </>
+        )
+    }
+};
+
+export const HideHeaderCloseIcon: Story = {
+    name: "Show Close Icon",
+    args: {
+        isOpen: false,
+        onRequestClose: () => { },
+        ariaHideApp: false,
+        shouldCloseOnOverlayClick: true,
+    },
+    render : (args)=>{
+        const [isOpen, setIsOpen] = useState(args.isOpen);
+
+        useEffect(() => {
+            setIsOpen(args.isOpen);
+        }, [args.isOpen]);
+        return (
+            <>
+            <Button onClick={() => setIsOpen(true)}>Open Dialog</Button>
+            <Dialog
+                {...args}
+                isOpen={isOpen}
+                shouldCloseOnOverlayClick={args.shouldCloseOnOverlayClick}
+                onRequestClose={() => setIsOpen(false)}
+                >
+                <Dialog.Header hideCloseIcon={false} >title</Dialog.Header>
                 <Dialog.Body>body</Dialog.Body>
                 <Dialog.Footer>
                     <Button color="black">cancel</Button>
@@ -63,10 +160,27 @@ export const HideHeaderCloseIcon: Story = {
 
 export const NoHeader: Story = {
     name: "Hide Header",
-    render : ()=>{
+    args: {
+        isOpen: false,
+        onRequestClose: () => { },
+        ariaHideApp: false,
+        shouldCloseOnOverlayClick: true,
+    },
+    render : (args)=>{
+        const [isOpen, setIsOpen] = useState(args.isOpen);
+
+        useEffect(() => {
+            setIsOpen(args.isOpen);
+        }, [args.isOpen]);
         return (
             <>
-            <Dialog isOpen={true}>
+            <Button onClick={() => setIsOpen(true)}>Open Dialog</Button>
+            <Dialog
+                {...args}
+                isOpen={isOpen}
+                shouldCloseOnOverlayClick={args.shouldCloseOnOverlayClick}
+                onRequestClose={() => setIsOpen(false)}
+                >
                 <Dialog.Body>body</Dialog.Body>
                 <Dialog.Footer>
                     <Button color="black">cancel</Button>
@@ -78,14 +192,104 @@ export const NoHeader: Story = {
     }
 };
 
+
 export const NoFooter: Story = {
     name: "Hide Footer",
-    render : ()=>{
+    args: {
+        isOpen: false,
+        onRequestClose: () => { },
+        ariaHideApp: false,
+        shouldCloseOnOverlayClick: true,
+    },
+    render : (args)=>{
+        const [isOpen, setIsOpen] = useState(args.isOpen);
+
+        useEffect(() => {
+            setIsOpen(args.isOpen);
+        }, [args.isOpen]);
         return (
             <>
-            <Dialog isOpen={true}>
+            <Button onClick={() => setIsOpen(true)}>Open Dialog</Button>
+            <Dialog
+                {...args}
+                isOpen={isOpen}
+                shouldCloseOnOverlayClick={args.shouldCloseOnOverlayClick}
+                onRequestClose={() => setIsOpen(false)}
+                >
                 <Dialog.Header>title</Dialog.Header>
                 <Dialog.Body>body</Dialog.Body>
+            </Dialog>
+            </>
+        )
+    }
+};
+
+
+export const HeaderCloseIconHandler: Story = {
+    name: "Close Dialog when Click on Close Icon",
+    args: {
+        isOpen: false,
+        onRequestClose: () => { },
+        ariaHideApp: false,
+        shouldCloseOnOverlayClick: true,
+    },
+    render : (args)=>{
+        const [isOpen, setIsOpen] = useState(args.isOpen);
+
+        useEffect(() => {
+            setIsOpen(args.isOpen);
+        }, [args.isOpen]);
+        return (
+            <>
+            <Button onClick={() => setIsOpen(true)}>Open Dialog</Button>
+            <Dialog
+                {...args}
+                isOpen={isOpen}
+                shouldCloseOnOverlayClick={args.shouldCloseOnOverlayClick}
+                onRequestClose={() => setIsOpen(false)}
+                >
+                <Dialog.Header hideCloseIcon={false} onRequestClose={()=>setIsOpen(false)}>title</Dialog.Header>
+                <Dialog.Body>body</Dialog.Body>
+                <Dialog.Footer hideBorder>
+                    <Button color="black">cancel</Button>
+                    <Button>ok</Button>
+                </Dialog.Footer>
+            </Dialog>
+            </>
+        )
+    }
+};
+
+export const DisableEscButton: Story = {
+    name: "Disable ESC Button",
+    args: {
+        isOpen: false,
+        onRequestClose: () => { },
+        ariaHideApp: false,
+        shouldCloseOnOverlayClick: true,
+    },
+    render : (args)=>{
+        const [isOpen, setIsOpen] = useState(args.isOpen);
+
+        useEffect(() => {
+            setIsOpen(args.isOpen);
+        }, [args.isOpen]);
+        return (
+            <>
+            <Button onClick={() => setIsOpen(true)}>Open Dialog</Button>
+            <Dialog
+                {...args}
+                isOpen={isOpen}
+                shouldCloseOnOverlayClick={args.shouldCloseOnOverlayClick}
+                onRequestClose={() => setIsOpen(false)}
+                shouldCloseOnEsc={false}
+                >
+                <Dialog.Header>title</Dialog.Header>
+                <Dialog.Body>body</Dialog.Body>
+                <Dialog.Footer hideBorder>
+                    <Button color="black">cancel</Button>
+                    <Button>ok</Button>
+                </Dialog.Footer>
             </Dialog>
             </>
         )
