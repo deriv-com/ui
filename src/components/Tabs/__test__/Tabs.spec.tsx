b07ca7f234344ad5f69f5459bb93133b9db3fc4c
yaswanth-deriv <yaswanth@regentmarkets.com>
2024-03-29 14:47:07 +0400

    test: added new test cases for tab component

--- src/components/Tabs/__test__/Tabs.spec.tsx
+++ src/components/Tabs/__test__/Tabs.spec.tsx
@@ -13,25 +13,66 @@ describe("Tabs Component", () => {
         );
 
         const tabsComponent = container.querySelector(".derivs-primary-tabs")
+        const tabTitle = screen.getByText("Tab 1")
         const tab1Content = screen.getByText("Tab 1 Content")
+        expect(tabTitle).toBeInTheDocument();
         expect(tabsComponent).toBeInTheDocument();
         expect(tab1Content).toBeInTheDocument();
         expect(tabsComponent).toHaveClass("derivs-primary-tabs");
     });
 
-    it("changes active tab when clicked", () => {
+    it("changes active tab when clicked and checks active tab content showing", () => {
         render(
-            <Tabs>
+            <Tabs activeTab="Tab 1">
                 <Tab title="Tab 1">Tab 1 Content</Tab>
                 <Tab title="Tab 2">Tab 2 Content</Tab>
             </Tabs>
         );
+        const tab1 = screen.getByText("Tab 1 Content")
+        expect(tab1).toBeInTheDocument();
         const tab2 = screen.getByText("Tab 2");
         fireEvent.click(tab2);
         const activeButton = screen.getAllByRole("button")[1];
         const tabContent = screen.getByText("Tab 2 Content")
         expect(tabContent).toBeInTheDocument();
         expect(activeButton).toHaveClass("derivs-primary-tabs__btn--active");
+    });
+
+    it("should render tabs with correct variant", () => {
+        const { container } = render(
+            <Tabs variant="secondary">
+                <Tab title="Tab 1">Tab 1 Content</Tab>
+                <Tab title="Tab 2">Tab 2 Content</Tab>
+            </Tabs>
+        );
+        const tabsComponent = container.querySelector(".derivs-secondary-tabs")
+        expect(tabsComponent).toBeInTheDocument();
+    });
+
+    it("should render tabs with correct title font size", () => {
+        const { container } = render(
+            <Tabs TitleFontSize="lg">
+                <Tab title="Tab 1">Tab 1 Content</Tab>
+                <Tab title="Tab 2">Tab 2 Content</Tab>
+            </Tabs>
+        );
+
+        const tabsComponent = container.querySelector(".derivs-primary-tabs")
+
+        expect(tabsComponent).toHaveStyle("font-size: lg;");
+    });
+
+    it("should call onChange handler when a tab is clicked", () => {
+        const mockOnChange = jest.fn();
+        const { getByText } = render(
+            <Tabs onChange={mockOnChange}>
+                <Tab title="Tab 1">Tab 1 Content</Tab>
+                <Tab title="Tab 2">Tab 2 Content</Tab>
+            </Tabs>
+        );
+        const tabTitle = getByText("Tab 1");
+        fireEvent.click(tabTitle);
+        expect(mockOnChange).toHaveBeenCalled();
     });
 
     it("applies custom class to wrapper", () => {
