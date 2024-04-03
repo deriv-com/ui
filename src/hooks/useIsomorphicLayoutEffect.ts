ffe063b87a619a20dea800b0fadc9b44c54f08f1
shayan khaleghparast <iman@regentmarkets.com>
2024-04-03 11:36:43 +0800

    chore: installed usehooks-ts and removed the unnecessary hooks

--- src/hooks/useIsomorphicLayoutEffect.ts
+++
@@ -1,5 +0,0 @@
-// Code from: https://usehooks-typescript.com/react-hook/use-event-listener
-import { useEffect, useLayoutEffect } from "react";
-
-export const useIsomorphicLayoutEffect =
-    typeof window !== "undefined" ? useLayoutEffect : useEffect;
