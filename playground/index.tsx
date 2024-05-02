a12fd88c2507c8da5a9daa6b36265942018d4705
shayan khaleghparast <iman@regentmarkets.com>
2024-05-02 20:16:11 +0800

    feat: implement account switcher [WIP]

--- playground/index.tsx
+++ playground/index.tsx
@@ -2,6 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Header,Button } from "../src/main"
 
+const account_list = [
+  {
+      icon: "USD",
+      type: "US Dollar",
+      loginId: "CR900000702",
+      balance: "10,000.00",
+      currency: "USD",
+      token: "",
+      is_eu: true,
+  },
+  {
+      icon: "USDT",
+      type: "Tether TRC20",
+      loginId: "CR900000527",
+      balance: "60,000.00",
+      currency: "tUSDT",
+      token: "",
+      is_eu: false,
+  },
+  {
+      icon: "Demo",
+      type: "Demo",
+      loginId: "VRTC900000702",
+      balance: "10,000.00",
+      currency: "USD",
+      token: "",
+      is_eu: false,
+  },
+];
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Header>
@@ -13,7 +43,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Header.LeftWrapper>
         <Header.LeftWrapper.Notification />
         <Header.LeftWrapper.AccountSetting />
-        <Header.LeftWrapper.AccountSwitcher />
+        <Header.LeftWrapper.AccountSwitcher accounts_list={account_list}  />
         <Button size='sm'> Deposit </Button>
       </Header.LeftWrapper>
 
--- src/components/AppLayout2/Header/AccountSwitcher/index.tsx
+++ src/components/AppLayout2/Header/AccountSwitcher/index.tsx
@@ -1,7 +1,96 @@
-export const AccountSwitcher = () => {
-    return <div style={{ backgroundColor: "yellow" }}>
+import { Tabs, Tab } from "../../../Tabs";
+import { Accordion } from "../../../Accordion";
+import {
+    LegacyChevronDown2pxIcon,
+    CurrencyUsdIcon,
+    // CurrencyBtcIcon,
+    // CurrencyDemoIcon,
+    // CurrencyUsdtIcon,
+    // CurrencyEthIcon,
+} from "@deriv/quill-icons";
 
-    </div>
-}
+import "./AccountSwitcher.scss";
+
+type TAccount = {
+    icon: string;
+    type: string;
+    loginId: string;
+    balance: string;
+    currency: string;
+    token: string;
+    is_eu: boolean;
+};
+
+type AccountSwitcherProps = {
+    accounts_list: TAccount[];
+};
+
+// const getCurrencyIcon = (currency: string) => {
+//     const CurrencyIcons: {
+//         [key: string]: React.ForwardRefExoticComponent<
+//             React.SVGProps<SVGSVGElement>
+//         >;
+//     } = {
+//         usd: CurrencyBtcIcon,
+//         demo: CurrencyDemoIcon,
+//         btc: CurrencyBtcIcon,
+//         usdt: CurrencyUsdtIcon,
+//         eth: CurrencyEthIcon,
+//     };
+
+//     return CurrencyIcons[currency] || CurrencyUsdIcon;
+// };
+
+export const AccountSwitcher = ({ accounts_list }: AccountSwitcherProps) => {
+    return (
+        <div className="deriv-account-switcher">
+            <div className="deriv-account-switcher__currency-icon">
+                <CurrencyUsdIcon iconSize="sm" />
+            </div>
+            <div className="deriv-account-switcher__balance">0.00 USD</div>
+            <LegacyChevronDown2pxIcon iconSize="xs" />
+            <div className="deriv-account-switcher__dropdown">
+                <Tabs activeTab="Real" variant="secondary">
+                    <Tab title="Real">
+                        <div className="deriv-account-switcher__tab--real">
+                            <div className="account-switcher__accounts">
+                                <Accordion
+                                    title="Non-EU Deriv account"
+                                    defaultOpen
+                                >
+                                    {accounts_list.map((account) => (
+                                        <div className="deriv-account-switcher__account" key={account.loginId}>
+                                            <div className="icon">
+                                                {/* {getCurrencyIcon(account.icon)} */}
+                                            </div>
+                                        </div>
+                                    ))}
+                                </Accordion>
+                                <Accordion title="EU Deriv account">
+                                    account for EU
+                                </Accordion>
+                            </div>
+                            <div className="account-switcher__total-asset">
+                                total asset
+                            </div>
+                            <div className="account-switcher__cfd">
+                                cfd link
+                            </div>
+                            <div className="account-switcher__footer">
+                                footer
+                            </div>
+                        </div>
+                    </Tab>
+                    <Tab
+                        title="Demo"
+                        className="deriv-account-switcher__tab--demo"
+                    >
+                        Demo Tab content
+                    </Tab>
+                </Tabs>
+            </div>
+        </div>
+    );
+};
 
 AccountSwitcher.displayName = "AccountSwitcher";
