28d9cb43d19857166d14cd55ad79b3680007b272
shayan khaleghparast <iman@regentmarkets.com>
2024-05-10 12:48:31 +0800

    chore: improved onClickOutside functionality in contextMenu

--- src/components/ContextMenu/index.tsx
+++ src/components/ContextMenu/index.tsx
@@ -1,59 +1,61 @@
 import clsx from "clsx";
 import "./ContextMenu.scss";
-import { ComponentProps, PropsWithChildren, useEffect, useRef, useState } from "react";
+import {
+    ComponentProps,
+    PropsWithChildren,
+    forwardRef,
+    useEffect,
+    useRef,
+    useState,
+} from "react";
 
 type ContextMenuProps = ComponentProps<"div"> & {
     isOpen: boolean;
-    onClickOutside?: () => void;
 };
 
-export const ContextMenu = ({
-    children,
-    className,
-    isOpen,
-    onClickOutside,
-    ...rest
-}: PropsWithChildren<ContextMenuProps>) => {
-    const [hide, setHide] = useState(true);
-    const timerRef = useRef<ReturnType<typeof setInterval> | null>(null);
+export const ContextMenu = forwardRef<
+    HTMLDivElement,
+    PropsWithChildren<ContextMenuProps>
+>(
+    (
+        {
+            children,
+            className,
+            isOpen,
+            ...rest
+        }: PropsWithChildren<ContextMenuProps>,
+        ref,
+    ) => {
+        const [hide, setHide] = useState(true);
+        const timerRef = useRef<ReturnType<typeof setInterval> | null>(null);
+        useEffect(() => {
+            if (isOpen) setHide(false);
+            else {
+                timerRef.current = setTimeout(() => {
+                    setHide(!isOpen);
+                }, 400);
+            }
+            return () => {
+                timerRef.current && clearTimeout(timerRef.current);
+            };
+        }, [isOpen]);
 
-    useEffect(() => {
-        if (isOpen) setHide(false);
-        else {
-            timerRef.current = setTimeout(() => {
-                setHide(!isOpen);
-            }, 400);
-        }
-        return () => {
-            timerRef.current && clearTimeout(timerRef.current);
-        };
-    }, [isOpen]);
+        if (hide) return null;
 
-    if (hide) return null;
-
-    return (
-        <>
-            <div
-                data-testid="context-menu-overlay"
-                className={clsx(
-                    "deriv-context-menu__overlay",
-                    !isOpen && "deriv-fadeout",
-                )}
-                onClick={(e) => {
-                    e.stopPropagation();
-                    onClickOutside?.();
-                }}
-            />
-            <div
-                className={clsx(
-                    "deriv-context-menu",
-                    !isOpen && "deriv-fadeout",
-                    className,
-                )}
-                {...rest}
-            >
-                {children}
+        return (
+            <div ref={ref}>
+                <div
+                    className={clsx(
+                        "deriv-context-menu",
+                        !isOpen && "deriv-fadeout",
+                        className,
+                    )}
+                    onClick={(e) => e.stopPropagation()}
+                    {...rest}
+                >
+                    {children}
+                </div>
             </div>
-        </>
-    );
-};
+        );
+    },
+);
