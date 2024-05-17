e62ceff85d12c91bc4a4a57cb734f259d85bc36d
Ali(Ako) Hosseini <ali.hosseini@deriv.com>
2024-05-17 18:59:23 +0800

    feat: hide hover stlye on language

--- src/components/AppLayout/LanguagesSwitcher/LanguageItem.tsx
+++ src/components/AppLayout/LanguagesSwitcher/LanguageItem.tsx
@@ -23,6 +23,7 @@ export const LanguageItem = ({
         icon={language.icon}
         onClick={onButtonClick}
         variant="ghost"
+        hideHoverStyles={true}
     >
         <Text
             size="sm"
