bd5ced810ba5672621cdd0b7132c9832965723d1
Ali(Ako) Hosseini <ali.hosseini@deriv.com>
2024-05-17 13:39:49 +0800

    feat: add language mobile drawer

---
+++ src/components/AppLayout/LanguagesModal/MobileLanguagesDrawer.tsx
@@ -0,0 +1,67 @@
+import { ReactElement } from "react";
+import { Button } from "../../Button";
+import { Text } from "../../Text";
+import "./LanguagesDrawer.scss";
+import clsx from "clsx";
+export type TLanguageItem = {
+    code: string;
+    display_name: string | ReactElement;
+    icon: ReactElement;
+};
+
+type TLanguageDrawer = {
+    onClose: () => void;
+    onLanguageSwitch: (code: string) => void;
+    selectedLang: string;
+    className: string;
+    isOpen: boolean;
+    wrapperClassName?: string;
+};
+
+// Define the props for the component using intersection type
+type TLanguagesItemsDrawerProps = {
+    languages: TLanguageItem[];
+} & TLanguageDrawer;
+
+export const MobileLanguagesDrawer = ({
+    languages,
+    onClose,
+    onLanguageSwitch,
+    selectedLang,
+    isOpen,
+    wrapperClassName
+}: TLanguagesItemsDrawerProps) => {
+    return (
+        <div className={clsx("languages-drawer", wrapperClassName)}>
+            {isOpen &&
+                languages.map((language) => {
+                    const Icon = () => language.icon;
+                    const onButtonClick = () => {
+                        onLanguageSwitch(language.code);
+                        onClose();
+                    };
+                    return (
+                        <div key={language.code} className={"languages-drawer__item"}>
+                            <Button
+                                className="languages-drawer__item-button"
+                                icon={<Icon />}
+                                onClick={onButtonClick}
+                                variant="ghost"
+                            >
+                                <Text
+                                    size="sm"
+                                    weight={
+                                        selectedLang === language.code
+                                            ? "bold"
+                                            : "normal"
+                                    }
+                                >
+                                    {language.display_name}
+                                </Text>
+                            </Button>
+                        </div>
+                    );
+                })}
+        </div>
+    );
+};
