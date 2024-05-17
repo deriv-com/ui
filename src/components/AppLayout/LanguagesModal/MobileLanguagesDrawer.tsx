19a21a8c9c05153d8d66d9fb1cdf7943282f1701
Ali(Ako) Hosseini <ali.hosseini@deriv.com>
2024-05-17 15:28:21 +0800

    feat: use language item component

--- src/components/AppLayout/LanguagesModal/MobileLanguagesDrawer.tsx
+++ src/components/AppLayout/LanguagesModal/MobileLanguagesDrawer.tsx
@@ -1,19 +1,11 @@
-import { ReactElement } from "react";
-import { Button } from "../../Button";
-import { Text } from "../../Text";
 import "./LanguagesDrawer.scss";
 import clsx from "clsx";
-export type TLanguageItem = {
-    code: string;
-    display_name: string | ReactElement;
-    icon: ReactElement;
-};
+import { LanguageItem, TLanguageItem } from "./LanguageItem";
 
 type TLanguageDrawer = {
     onClose: () => void;
     onLanguageSwitch: (code: string) => void;
     selectedLang: string;
-    className: string;
     isOpen: boolean;
     wrapperClassName?: string;
 };
@@ -29,37 +21,23 @@ export const MobileLanguagesDrawer = ({
     onLanguageSwitch,
     selectedLang,
     isOpen,
-    wrapperClassName
+    wrapperClassName,
 }: TLanguagesItemsDrawerProps) => {
     return (
-        <div className={clsx("languages-body", wrapperClassName)}>
+        <div className={clsx("languages__body", wrapperClassName)}>
             {isOpen &&
                 languages.map((language) => {
-                    const Icon = () => language.icon;
                     const onButtonClick = () => {
                         onLanguageSwitch(language.code);
                         onClose();
                     };
                     return (
-                        <div key={language.code} className="languages-body__item">
-                            <Button
-                                className="languages-body__item-button"
-                                icon={<Icon />}
-                                onClick={onButtonClick}
-                                variant="ghost"
-                            >
-                                <Text
-                                    size="sm"
-                                    weight={
-                                        selectedLang === language.code
-                                            ? "bold"
-                                            : "normal"
-                                    }
-                                >
-                                    {language.display_name}
-                                </Text>
-                            </Button>
-                        </div>
+                        <LanguageItem
+                            key={language.code}
+                            language={language}
+                            onButtonClick={onButtonClick}
+                            currentLang={selectedLang}
+                        />
                     );
                 })}
         </div>
