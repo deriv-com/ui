1d59883e3b291f42867f81587bb142a2b50a3bbe
Ali(Ako) Hosseini <ali.hosseini@deriv.com>
2024-05-17 13:39:21 +0800

    feat: add desktop lang modal

---
+++ src/components/AppLayout/LanguagesModal/DesktopLanguagesModal.tsx
@@ -0,0 +1,54 @@
+import React, { ReactElement } from "react";
+import { Modal, Text } from "../../../main";
+import "./LanguagesModal.scss";
+import { IconTypes } from "@deriv/quill-icons";
+import { LanguageItem } from "./LanguageItem";
+
+type TLanguagesModalProps = {
+    isModalOpen: boolean;
+    languages: {
+        code: string;
+        display_name: string | ReactElement;
+        icon: IconTypes;
+    }[];
+    onClose: () => void;
+    onLanguageSwitch: (code: string) => void;
+    currentLang: string;
+    headerTitle: string | ReactElement;
+};
+
+export const DesktopLanguagesModal = ({
+    isModalOpen,
+    languages,
+    onClose,
+    onLanguageSwitch,
+    currentLang,
+    headerTitle,
+}: TLanguagesModalProps) => {
+    return (
+        <Modal
+            ariaHideApp={false}
+            className="languages-modal"
+            isOpen={isModalOpen}
+        >
+            <Modal.Header hideBorder onRequestClose={onClose}>
+                <Text weight="bold">{headerTitle}</Text>
+            </Modal.Header>
+            <Modal.Body className="languages-modal__body">
+                {languages.map((language) => {
+                    return (
+                        <LanguageItem
+                            language={language}
+                            currentLang={currentLang}
+                            key={language.code}
+                            onButtonClick={() => {
+                                onLanguageSwitch(language.code);
+                                onClose();
+                            }}
+                        />
+                    );
+                })}
+            </Modal.Body>
+        </Modal>
+    );
+};
