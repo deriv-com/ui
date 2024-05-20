13112d6d584d87cdce50479ac14bdb63833595c5
Niloofar <niloofar.sadeghi@regentmarkets.com>
2024-05-20 15:57:50 +0800

    fix: tooltip issue

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -1,29 +1,55 @@
-import React, { ReactNode, useRef, useState } from "react";
+import {
+    ComponentProps,
+    MouseEventHandler,
+    PropsWithChildren,
+    useRef,
+    useState,
+} from "react";
 import clsx from "clsx";
 import { useOnClickOutside } from "usehooks-ts";
 import "./Tooltip.scss";
 
-type TooltipPositionType = "top" | "bottom" | "left" | "right";
-type TooltipTriggerActionType = "hover" | "click";
-type TooltipVariantType = "general" | "dark" | "error";
-
-type TooltipProps = {
-    children?: ReactNode;
-    className?: string;
-    message: ReactNode;
-    position?: TooltipPositionType;
-    triggerAction?: TooltipTriggerActionType;
-    variant?: TooltipVariantType;
+type TTooltip = Omit<
+    ComponentProps<"div">,
+    "ref" | "onMouseEnter" | "onMouseLeave"
+> & {
+    message: string;
+    position?: "top" | "bottom" | "left" | "right";
+    triggerAction?: "hover" | "click";
+    variant?: "general" | "dark" | "error";
+    activeClassName?: HTMLDivElement["className"];
 };
+
+const TooltipVariantClass = {
+    dark: "deriv-tooltip--dark",
+    error: "deriv-tooltip--error",
+    general: "deriv-tooltip--general",
+} as const;
+
+const TooltipPositionClass = {
+    top: "deriv-top",
+    bottom: "deriv-bottom",
+    right: "deriv-right",
+    left: "deriv-left",
+} as const;
+
+const TooltipArrowVariantClass = {
+    dark: "deriv-arrow--dark",
+    error: "deriv-arrow--error",
+    general: "deriv-arrow--general",
+} as const;
 
 export const Tooltip = ({
     children,
     className,
+    activeClassName,
     message,
     position = "top",
     triggerAction = "hover",
     variant = "general",
-}: TooltipProps) => {
+    onClick,
+    ...rest
+}: PropsWithChildren<TTooltip>) => {
     const [active, setActive] = useState(false);
     const targetRef = useRef<HTMLDivElement>(null);
 
@@ -39,13 +65,17 @@ export const Tooltip = ({
         }
     };
 
-    const handleClick = () => {
+    const handleClick: MouseEventHandler<HTMLDivElement> = (event) => {
         if (triggerAction === "click") {
             setActive(!active);
         }
+
+        onClick?.(event);
     };
 
-    const handleClickOutside = (event: MouseEvent | TouchEvent | FocusEvent) => {
+    const handleClickOutside = (
+        event: MouseEvent | TouchEvent | FocusEvent,
+    ) => {
         if (
             triggerAction === "click" &&
             active &&
@@ -58,41 +88,24 @@ export const Tooltip = ({
 
     useOnClickOutside(targetRef, (e) => handleClickOutside(e));
 
-    const TooltipVariantClass: Record<TooltipVariantType, string> = {
-        dark: "deriv-tooltip--dark",
-        error: "deriv-tooltip--error",
-        general: "deriv-tooltip--general",
-    };
-
-    const TooltipPositionClass: Record<TooltipPositionType, string> = {
-        top: "deriv-top",
-        bottom: "deriv-bottom",
-        right: "deriv-right",
-        left: "deriv-left",
-    };
-
-    const TooltipArrowVariantClass: Record<TooltipVariantType, string> = {
-        dark: "deriv-arrow--dark",
-        error: "deriv-arrow--error",
-        general: "deriv-arrow--general",
-    };
-
     return (
         <div
-            className="deriv-tooltip-container"
+            ref={targetRef}
+            className={clsx("deriv-tooltip-container", className)}
+            onClick={handleClick}
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
-            onClick={handleClick}
-            ref={targetRef}
+            {...rest}
         >
             {children}
+
             {active && (
                 <div
                     className={clsx(
                         "deriv-tooltip",
                         TooltipPositionClass[position],
                         TooltipVariantClass[variant],
-                        className,
+                        activeClassName,
                     )}
                 >
                     <span
