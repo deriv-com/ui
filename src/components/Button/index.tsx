362471dffb684790d17e8a6a38db1963d334a605
Jim Daniels Wasswa <jim@regentmarkets.com>
2024-07-22 11:44:16 +0800

    chore: add primary-transparent to loader

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,7 +6,7 @@ import { Text } from "../Text";
 import "./Button.scss";
 
 type TVariant = "contained" | "ghost" | "outlined";
-type TColor = "black" | "primary-light" | "primary-transparent" | "primary" | "white";
+type TColor = "black" | "primary" | "primary-light" | "primary-transparent" | "white";
 type TBorderWidth = Extract<TGenericSizes, 'md' | 'sm' | 'lg'> | 'none';
 
 interface ButtonProps extends ComponentProps<"button"> {
@@ -58,6 +58,7 @@ const LoaderColor = {
     black: "#333333",
     primary: "#FFFFFF",
     "primary-light": "#EC3F3F",
+    "primary-transparent": "#ff444f14",
     white: "#85ACB0",
 } as const;
 
