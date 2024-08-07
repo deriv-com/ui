5b5e7f273eec2f0800070dce1d4ad94cbf2e2782
ameerul-deriv <ameerul@deriv.com>
2024-08-07 14:06:48 +0800

    chore: added debounce to not spam the api calls on scroll

--- src/hooks/useFetchMore.ts
+++ src/hooks/useFetchMore.ts
@@ -1,4 +1,5 @@
 import { RefObject, useCallback, useEffect } from "react";
+import debounce from "lodash/debounce";
 
 type TProps = {
     loadMore: () => void;
@@ -7,6 +8,7 @@ type TProps = {
 
 /** A custom hook to load more items in the table on scroll to bottom of the table */
 export const useFetchMore = ({ loadMore, ref }: TProps) => {
+    const debouncedLoadMore = debounce(loadMore, 100);
     //called on scroll and possibly on mount to fetch more data as the user scrolls and reaches bottom of table
     const fetchMoreOnBottomReached = useCallback(
         (containerRefElement?: HTMLDivElement | null) => {
@@ -14,14 +16,14 @@ export const useFetchMore = ({ loadMore, ref }: TProps) => {
                 const { clientHeight, scrollHeight, scrollTop } =
                     containerRefElement;
                 //once the user has scrolled within 200px of the bottom of the table, fetch more data if we can
-                const isBottom = scrollHeight - scrollTop <= clientHeight + 200;
+                const isBottom = scrollHeight - scrollTop - clientHeight < 200;
 
                 if (isBottom) {
-                    loadMore();
+                    debouncedLoadMore();
                 }
             }
         },
-        [loadMore],
+        [debouncedLoadMore],
     );
 
     useEffect(() => {
