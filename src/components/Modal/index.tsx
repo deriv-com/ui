833b45ea0e7ec71770df78c930f56ac00bb12a73
mayuran-deriv <balamayuran@deriv.com>
2024-11-26 11:51:29 +0400

    fix: comments

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -11,17 +11,17 @@ export const CUSTOM_STYLES = {
     content: {
         background: 'none',
         border: 'none',
-        borderRadius: '10px',
+        borderRadius: 0,
         bottom: 'auto',
         left: '50%',
         margin: 0,
         marginRight: '-50%',
         padding: 0,
         right: 'auto',
-        top: '20%',
+        top: '50%',
         transform: 'translate(-50%, -50%)',
         transition: 'all 0.35s ease-in-out',
-        boxShadow: '0px 32px 64px 0px rgba(14, 14, 14, 0.14)',
+        boxShadow: '0px 32px 64px 0px rgba(14, 14, 14, 0.14)'
     },
     overlay: {
         backgroundColor: 'rgba(0, 0, 0, 0.72)',
